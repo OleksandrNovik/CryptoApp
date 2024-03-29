44d69ddca84f0c8d4a5400d8b9a0328b60d399eb
Novik Oleksandr <novikoleksandr9@gmail.com>
2024-03-29 17:48:19 +0200

    Made navigation buttons for new windows to open

--- Components/HeaderRelated/Header.xaml.cs
+++ Components/HeaderRelated/Header.xaml.cs
@@ -1,13 +1,15 @@
 ﻿using System.Windows.Controls;
 
-namespace TestTrainee.Components
+namespace TestTrainee.Components.HeaderRelated
 {
+    /// <summary>
+    /// Interaction logic for Header.xaml
+    /// </summary>
     public partial class Header : UserControl
     {
         public Header()
         {
             InitializeComponent();
         }
-
     }
 }
